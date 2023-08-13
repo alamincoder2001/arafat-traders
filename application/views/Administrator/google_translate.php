d2b14ae360fae419b70fbcb3dddf8d824990849d
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-08-13 16:41:23 +0600

    before work business monitor now product ledger

---
+++ application/views/Administrator/google_translate.php
@@ -0,0 +1,43 @@
+<script type="text/javascript">
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en', includedLanguages: 'en,bn'
+        }, 'google_translate_element');
+    }
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<style>
+    .goog-te-banner-frame {
+        display: none;
+    }
+    .navbar-fixed-top + .main-container {
+        padding-top: 5px;
+    }
+    .goog-tooltip {
+        display: none !important;
+    }
+    .goog-tooltip:hover {
+        display: none !important;
+    }
+    .goog-text-highlight {
+        background-color: transparent !important;
+        border: none !important; 
+        box-shadow: none !important;
+    }
+    .clock {
+        min-width: 300px !important;
+    }
+    .goog-te-gadget {
+        height: 40px;
+        overflow: hidden;
+        text-align: center;
+    }
+    .goog-te-gadget .goog-te-combo {
+        background: #438eb9;
+        border: none;
+        color: #fff;
+        font-weight: bold;
+    }
+</style>
